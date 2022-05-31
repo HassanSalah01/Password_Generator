de857e12459f69fdf20595b55bbd6c56386119c0
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-05-31 13:28:29 +0200

    second_commit_added_new_functions

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const print=(word)=>{
+    console.log(word)
+}
+
 const randLetter=()=>{
     return Math.floor(Math.random() * (127 - 33 + 1) + 33);
 }
@@ -15,4 +19,29 @@ const fromasciTodecimal = (asciArr)=>{
         return info.charCodeAt(0);
     })
 }
-console.log(fromasciTodecimal(randPass(50)))
+const fullPass = (passArr)=>{
+    strpass = "";
+    for(let i of passArr){
+        strpass+=i;
+    }
+    return strpass;
+}
+
+let x = ["a","h","m","e","d"];
+
+const testing=(arr,s)=>{
+    for (let i = 0; i < arr.length;i++){
+        arr[i]=String.fromCharCode(arr[i].charCodeAt(0)+s);
+    }
+    return arr;
+}
+
+const encry=(word,rev)=>{
+    let count = 0 ;
+    for(let i = 0 ; i < rev ; i++){
+        for (let j of word){
+            count+=j.charCodeAt(0);
+        }
+    }
+    return Math.floor((count/1000)/2);
+}
